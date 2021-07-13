b93b5ffde5f73700b4d54ab3c189283f5246cf6c
Pete Hamrick <pete.hamrick@gmail.com>
2021-07-13 14:32:12 -0700

    wrote pseudo code for renderRandomPokemon

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 // import functions and grab DOM elements
+import pokemon from './data/pokemon';
+
+const pokemon1Radio = document.getElementById('pokemon1-radio');
+const pokemon1Image = document.getElementById('pokemon1-img');
+
+const pokemon2Radio = document.getElementById('pokemon2-radio');
+const pokemon2Image = document.getElementById('pokemon2-img');
+
+const pokemon3Radio = document.getElementById('pokemon3-radio');
+const pokemon3Image = document.getElementById('pokemon3-img');
+
+const submitButton = document.getElementById('submit');
 
 // initialize state
+let totalPlays = 0;
 
+function renderRandomPokemon(){
+    // increment total plays
+    totalPlays++;
+    // generate 3 random indices of our pokemon array
+    // keep generating random numbers until they don't match
+
+    // update the shown key(increment counter) for each pokemon getPokedex(<id>)
+
+    // render the random pokemon
+}
 // set event listeners 
   // get user input
   // use user input to update state 
